1b6dc6d302e24f2e186df47fb76584cbb41812a2
HobbesWSL <jessewilson@outlook.com>
2022-04-18 19:59:16 +1200

    Add people model functions

---
+++ server/models/people.js
@@ -0,0 +1,20 @@
+// Imports
+const devDB = require("../db/devDB");
+
+// add people given an array of objects (names/urls), and group id
+function addPeople(cohort_id, people, db = devDB) {
+  return db("people").insert(
+    people.map((person) => ({ ...person, cohort_id }))
+  );
+}
+
+// get all people given a cohort id
+function getPeople(cohort_id, db = devDB) {
+  return db("people").select().where("cohort_id", cohort_id);
+}
+
+// exports
+module.exports = { addPeople, getPeople };
+
+// To do
+//  update Person
