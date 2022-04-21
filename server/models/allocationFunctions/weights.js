f55ae2d9c326876680a2ac44df17a31e8b993e66
HobbesWSL <jessewilson@outlook.com>
2022-04-21 14:27:21 +1200

    seperate weights module and tests

---
+++ server/models/allocationFunctions/weights.js
@@ -0,0 +1,61 @@
+// Imports
+const { range } = require("lodash");
+const {
+  createArray,
+  createArray2D,
+  copyArray2D,
+  forEachPair,
+} = require("../../utils");
+
+// Export
+module.exports = { padWeights, stopDoublePlaceholder, calculateNewWeights };
+
+// Functions
+
+// Add 0 padding to 2d array
+// Takes:
+//  - requiredSize: int (number rows/cols the final array should have)
+//  - weights: 2d array
+// Returns:
+//  - paddingArray: 2d array (of size requiredSize x requiredSize)
+function padWeights(requiredSize, weights) {
+  const toAdd = requiredSize - weights.length;
+  return [
+    ...weights.map((row) => [...row, ...createArray(toAdd)]),
+    ...createArray2D(toAdd, requiredSize),
+  ];
+}
+
+// Change to weights of two placeholders to be Infinity
+//  So that no group will ever has 2 placeholders/empty slots
+// Takes:
+//  - actualNumberPeople: int
+//  - weights: 2d array
+// Returns
+//  - updatedWeights: 2d array
+function stopDoublePlaceholder(actualNumberPeople, weights) {
+  const newWeights = copyArray2D(weights);
+  const placeHolders = range(actualNumberPeople, newWeights.length);
+  forEachPair(placeHolders, (a, b) => {
+    newWeights[a][b] = newWeights[b][a] = Infinity;
+  });
+  return newWeights;
+}
+
+// Calculate new weights based on allocation
+//  This should add one for each paring allocated
+// Takes:
+//  - weights: 2d array (old weights used to sore allocation)
+//  - allocation: 2d array (with placeholders removed)
+// Returns
+//  - newWeights: 2d array (updated with allocation parings)
+function calculateNewWeights(weights, allocation) {
+  let newWeights = copyArray2D(weights);
+  allocation.forEach((team) => {
+    forEachPair(team, (person1, person2) => {
+      newWeights[person1][person2] = newWeights[person2][person1] =
+        weights[person1][person2] + 1;
+    });
+  });
+  return newWeights;
+}
---
+++ server/models/allocationFunctions/weights.test.js
@@ -0,0 +1,119 @@
+// Imports
+const {
+  padWeights,
+  stopDoublePlaceholder,
+  calculateNewWeights,
+} = require("./weights");
+
+describe("padWeights function - adds 0 padding to a 2d array", () => {
+  it("Returned array has the correct dimensions", () => {
+    const requiredSize = 4;
+    const weights = [
+      [0, 0],
+      [0, 0],
+    ];
+    const actual = padWeights(requiredSize, weights);
+    expect.assertions(3);
+    expect(Array.isArray(actual)).toBe(true);
+    expect(actual.length).toBe(4);
+    expect(actual[0].length).toBe(4);
+  });
+  it("Returned array should keep original values", () => {
+    const requiredSize = 4;
+    const weights = [
+      [1, 2],
+      [3, 4],
+    ];
+    const actual = padWeights(requiredSize, weights);
+    const actualSum = actual
+      .flat()
+      .reduce((store, current) => store + current, 0);
+    const expectedSum = 10;
+    expect.assertions(2);
+    expect(actual[1][1]).toBe(4);
+    expect(actualSum).toBe(expectedSum);
+  });
+});
+
+describe("stopDoublePlaceholder - changes weight of two placeholders to be Infinity", () => {
+  it("Returns new array of same shape", () => {
+    const actualNumberPeople = 1;
+    const weights = [
+      [1, 1],
+      [1, 1],
+    ];
+    const actual = stopDoublePlaceholder(actualNumberPeople, weights);
+    expect.assertions(3);
+    expect(Array.isArray(actual)).toBe(true);
+    expect(actual.length).toBe(2);
+    expect(actual[0].length).toBe(2);
+  });
+  it("Intersection between placeholders should be Infinity", () => {
+    const actualNumberPeople = 1;
+    const weights = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
+    const actual = stopDoublePlaceholder(actualNumberPeople, weights);
+    expect(actual[1][2]).toBe(Infinity);
+    expect(actual[2][1]).toBe(Infinity);
+    expect(actual[0][0]).toBe(0);
+  });
+});
+
+describe("calculateNewWeights - returns new weights array updated based on allocation", () => {
+  it("Returns array of the correct size", () => {
+    const weights = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    const allocation = [
+      [0, 1],
+      [2, 3],
+    ];
+    const actual = calculateNewWeights(weights, allocation);
+    expect.assertions(3);
+    expect(Array.isArray(actual)).toBe(true);
+    expect(actual.length).toBe(4);
+    expect(actual[0].length).toBe(4);
+  });
+  it("Doesn't change non-pairs", () => {
+    const weights = [
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    const allocation = [
+      [0, 1],
+      [2, 3],
+    ];
+    const actual = calculateNewWeights(weights, allocation);
+    expect.assertions(4);
+    expect(actual[0][2]).toBe(1);
+    expect(actual[0][2]).toBe(1);
+    expect(actual[1][3]).toBe(0);
+    expect(actual[3][1]).toBe(0);
+  });
+  it("Adds 1 to pairs", () => {
+    const weights = [
+      [0, 2, 1, 0],
+      [2, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+    ];
+    const allocation = [
+      [0, 1],
+      [2, 3],
+    ];
+    const actual = calculateNewWeights(weights, allocation);
+    expect.assertions(4);
+    expect(actual[0][1]).toBe(3);
+    expect(actual[1][0]).toBe(3);
+    expect(actual[2][3]).toBe(1);
+    expect(actual[3][2]).toBe(1);
+  });
+});
